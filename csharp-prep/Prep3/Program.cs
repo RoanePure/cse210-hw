a54ced3bb5ba9d21f61c75a43fb43d0141e705e9
RoanePure <roane.pure01@gmail.com>
2024-09-21 22:21:57 +0800

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Generate random number 1-100
+        Random random = new Random();
+        int MagicNumber = random.Next(1, 101);
+
+        int guess = -1;
+
+        // Looping until guessed correctly
+        while (guess != MagicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string GuessInput = Console.ReadLine();
+            guess = int.Parse(GuessInput);
+
+            // Determine if guessed number is lower or higher or correct
+            if (guess < MagicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > MagicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it correctly!");
+            }
+        }
     }
 }
